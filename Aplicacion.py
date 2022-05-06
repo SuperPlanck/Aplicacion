4435b7447f61d13e7c78911f0f5c1a1017f656bf
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 09:25:24 -0600

    ,

--- Aplicacion.py
+++ Aplicacion.py
@@ -10,7 +10,7 @@ Titulo_principal = '<p style="font-family:TimesNewRoman; color:DarkBlue; font-si
 st.markdown(Titulo_principal, unsafe_allow_html=True)
 
 with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
+    add_header = st.header(
+        "Choose a shipping method"
+        
     )
