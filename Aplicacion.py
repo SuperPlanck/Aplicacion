fc7e48f4b1f65102659fca44c2899f995b97b5b9
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 09:05:02 -0600

    m

--- Aplicacion.py
+++ Aplicacion.py
@@ -4,9 +4,9 @@ import numpy as np
 
 original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Original image</p>'
 st.markdown(original_title, unsafe_allow_html=True)
-st.image(image, channels="BGR")
+
 
 new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">New image</p>'
 st.markdown(new_title, unsafe_allow_html=True)
-st.image(image, channels="BGR")
+
 st.title("Óptica Cuántica")
