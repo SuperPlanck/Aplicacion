9956ef91655a0cab5fd064e6c2618a71b63b9072
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 10:08:59 -0600

    m

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
+streamlit_style = "dd""
+			<style>
+			@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
 
+			html, body, [class*="css"]  {
+			font-family: 'Roboto', sans-serif;
+			}
+			</style>
+			"""
+st.markdown(streamlit_style, unsafe_allow_html=True)
 Titulo_concurso = '<p style="text-align:center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
 st.markdown(Titulo_concurso, unsafe_allow_html=True)
 
