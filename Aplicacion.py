6c0063e1f4bf9823e132d742de4d730851e59e92
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 09:08:11 -0600

    m

--- Aplicacion.py
+++ Aplicacion.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Original image</p>'
+original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Hackaton</p>'
 st.markdown(original_title, unsafe_allow_html=True)
 
 
