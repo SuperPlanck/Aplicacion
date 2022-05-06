429663ce164076f9c474e1a8632c0f829dc10652
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 09:18:14 -0600

    n

--- Aplicacion.py
+++ Aplicacion.py
@@ -2,11 +2,11 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-original_title = '<p style="text-align:center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
-st.markdown(original_title, unsafe_allow_html=True)
+Titulo_concurso = '<p style="text-align:center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
+st.markdown(Titulo_concurso, unsafe_allow_html=True)
 
 
-new_title = '<p style="font-family:TimesNewRoman; color:DarkBlue; font-size: 42px;">Óptica Cuántica</p>'
-st.markdown(new_title, unsafe_allow_html=True)
+Titulo_principal = '<p style="font-family:TimesNewRoman; color:DarkBlue; font-size: 42px;">Óptica Cuántica</p>'
+st.markdown(Titulo_principal, unsafe_allow_html=True)
 
 
