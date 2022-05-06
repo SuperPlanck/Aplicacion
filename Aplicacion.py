362736ed3edff7f3e0e59ab96ea8bd0b07c15598
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 10:05:54 -0600

    m

--- Aplicacion.py
+++ Aplicacion.py
@@ -2,13 +2,6 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-[theme]
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif"
-
 Titulo_concurso = '<p style="text-align:center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
 st.markdown(Titulo_concurso, unsafe_allow_html=True)
 
