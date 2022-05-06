1bd206415efb9b56b9d03aee4527e6f0bb0ab22a
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 11:34:23 -0600

    k

--- Aplicacion.py
+++ Aplicacion.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-
 Titulo_concurso = '<p style="text-align:center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
 st.markdown(Titulo_concurso, unsafe_allow_html=True)
 
