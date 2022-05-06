f695c9451ebc9362aeda9155467bb78959697379
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 09:15:44 -0600

    m

--- Aplicacion.py
+++ Aplicacion.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-original_title = '<p style="text-align=center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
+original_title = '<p style="text-align:center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
 st.markdown(original_title, unsafe_allow_html=True)
 
 
