d29d58211614dad28adb8126cbbc72c6566e9bba
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 12:18:35 -0600

    m

--- Aplicacion.py
+++ Aplicacion.py
@@ -33,3 +33,5 @@ with col3:
 st.markdown('Newton, principal aportador de la teoria de la luz estipulaba que las partículas de luz provenian de una fuente luminosa que estimulaba la vista.')
 st.markdown('Esta idea le permitió justificar las bases de la refracción y reflexión')
 st.markdown('Durante su vida sostuvó que la luz podría seguir un movimiento ondulatorio lo cual fue demostrado por Thomas Young, que bajo condiciones de difracción los rayos de luz interfieren unos con otros al igual que con las ondas mecánicas')
+
+st.markdown("![Alt Text](hhttp://blog.soton.ac.uk/soundwaves/files/2013/12/light1.gif)")
