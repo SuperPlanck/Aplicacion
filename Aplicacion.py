e68cb32f560623c81d9883fe447a54d2c9db6af2
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-07 12:02:02 -0600

    g

--- Aplicacion.py
+++ Aplicacion.py
@@ -98,7 +98,7 @@ if Temas == 'Introducción':
     st.image("https://slideplayer.es/slide/5497387/17/images/12/LEY+DE+RAYLEIGH-JEANS+Cat%C3%A1strofe+ultravioleta.jpg")
     st.markdown("Planck observó que la materia absorbía o emitía la energia en multiplos enteros del valor de la constante de Planck, pequeños paquetes conocidos como cuántos")
     st.latex('h=6.626^{-34}J')
-    st.markdown("Los fotones pueden ser emitidos o absorbidos por los átomos, se absorben por completo y al momento de que el átomo pierde energía se emite, cargando con la energía exacta que se perdió la cual es directamente proporcional a su frecuencia.")
+    st.markdown("Los fotones pueden ser emitidos o absorbidos por los átomos, se absorben por completo y al momento de que el átomo pierde energía se emiten, cargando con la energía exacta que se perdió la cual es directamente proporcional a su frecuencia.")
     st.markdown("Eso demostró que la luz se comporta también como una partícula")
     st.markdown("Esta relación es descrita por la famosa **Ecuación de Planck**:")
     st.latex('E=hf')
