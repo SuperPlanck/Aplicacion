d405d19111ede34dc391cd6ffb135bd641e70915
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 18:58:29 -0600

    .

--- Aplicacion.py
+++ Aplicacion.py
@@ -68,24 +68,24 @@ if Introduccion:
 
     st.markdown("Las ondas electromagnéticas pueden ser clasificadas por su longitud de onda o su frecuencia; esta clasificación se llama **Espectro electromagnético**")
 longitud = st.slider('Longitud de onda visible en nm',380,750,380)
-    if longitud < 450:
-        st.markdown('Color Morado')
-        st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Morado.png/250px-Morado.png",width=200)
-    if 450 < longitud < 495:
-        st.markdown('Color Azul')
-        st.image("https://upload.wikimedia.org/wikipedia/commons/5/52/Tipos_de_azules.png",width=200)
-    if 495 < longitud < 570:
-        st.markdown('Color Verde')
-        st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Tipos_de_verde.png/250px-Tipos_de_verde.png",width=200)
-    if 570 < longitud < 590:
-        st.markdown('Color Amarillo')
-        st.image("https://upload.wikimedia.org/wikipedia/commons/5/5b/Amarillos.png",width=200)
-    if 590 < longitud < 620:
-        st.markdown('Color Naranja')
-        st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Shades_of_orange.png/250px-Shades_of_orange.png",width=200)
-    if 620 < longitud < 750:
-        st.markdown('Color Rojo')
-        st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Rojos.png/250px-Rojos.png",width=200)
+if longitud < 450:
+      st.markdown('Color Morado')
+     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Morado.png/250px-Morado.png",width=200)
+if 450 < longitud < 495:
+     st.markdown('Color Azul')
+     st.image("https://upload.wikimedia.org/wikipedia/commons/5/52/Tipos_de_azules.png",width=200)
+if 495 < longitud < 570:
+     st.markdown('Color Verde')
+     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Tipos_de_verde.png/250px-Tipos_de_verde.png",width=200)
+if 570 < longitud < 590:
+     st.markdown('Color Amarillo')
+    st.image("https://upload.wikimedia.org/wikipedia/commons/5/5b/Amarillos.png",width=200)
+if 590 < longitud < 620:
+     st.markdown('Color Naranja')
+     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Shades_of_orange.png/250px-Shades_of_orange.png",width=200)
+if 620 < longitud < 750:
+    st.markdown('Color Rojo')
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Rojos.png/250px-Rojos.png",width=200)
 
     st.markdown("No se entendía muy bien el concepto de luz hasta 1900 cuando el físico Max Planck empezó a estudiar cuerpos negros(Objetos teóricos que absorven la energía radiante totalmente), estos se calentaban y empezaban a brillar.")
     st.image("http://nuclear-power.com/wp-content/uploads/blackbody-radiation-chart-min.png",width=700)
