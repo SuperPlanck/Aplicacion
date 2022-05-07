cd753252f3febf7c650d5d005272d71984a931fd
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-06 19:36:05 -0600

    .

--- Aplicacion.py
+++ Aplicacion.py
@@ -14,8 +14,9 @@ Titulo_principal = '<p style="font-family:TimesNewRoman; color:DarkBlue; font-si
 st.markdown(Titulo_principal, unsafe_allow_html=True)
 
 st.caption('Esta aplicacón fue hecha con el propósito de introducir hacia los fenómenos cuánticos, especificamente dentro de la rama de la óptica cuántica, que ha sido de dificil comprensión para la cual nuestro objetivo es demostrar tales fenómenos con ejemplos visuales y prácticos de entender')
-page_names = ['Introduccion', 'Foto']
+
 with st.sidebar:
+    page_names = ['Introduccion', 'Foto']
     Temas = st.radio("Temas",page_names)
       
 
