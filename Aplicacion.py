f545a1172f2fe133ce6fd77be66ba0f53d5f06cd
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-07 15:47:41 -0600

    .

--- Aplicacion.py
+++ Aplicacion.py
@@ -125,6 +125,7 @@ if Temas == 'Efecto Fotoeléctrico':
     st.write("Con base en estos descubrimientos, Einstein propuso que la luz se comportaba como una corriente de partículas llamadas fotones con una energía de:")
     st.latex("E= hv")
     st. write("La energía de un fotón es proporcional a la frecuencia de la luz (f) por lo que la amplitud de la luz (h) es proporcional al número de fotones con una frecuencia dada.")
+    
     col1, col2, col3 = st.columns([1,6,1])
 
     with col1:
@@ -138,6 +139,7 @@ if Temas == 'Efecto Fotoeléctrico':
     
     st.subheader("Bandgap")
     st.write("El bandgap o tambien conocido como banda prohibida es la energía mínima necesaria para excitar un electrón desde su estado ligado a un estado libre que le permita participar en la conducción, dentro de una celda solar. Lo importante a resaltar aqui es el intervalo de banda es la cantidad mínima de energía necesaria para un electrón de liberarse de su estado de enlace. Cuando se cumple la energía de banda prohibida, el electrón es excitado a un estado libre, y por lo tanto puede participar en la conducción.La brecha de banda determina la cantidad de energía que se necesita del sol para la conducción, así como la cantidad de energía que se genera.Un agujero se crea donde el electrón estaba obligado anteriormente. Este agujero también participa en la conducción.")
+    
     if st.button("Ver tabla"):
         col1, col2, col3 = st.columns([1,6,1])
         with col1:
