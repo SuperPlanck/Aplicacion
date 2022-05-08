84bae13b0aa66b4b89baaf30c73a5c34f8e835fc
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-07 18:11:02 -0600

    l

--- Aplicacion.py
+++ Aplicacion.py
@@ -5,7 +5,12 @@ import streamlit as st
 import numpy as np
 st.set_page_config(page_title="Óptica Cuántica",page_icon="🎇")
 
+Titulo_concurso = '<p style="text-align:center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
+st.markdown(Titulo_concurso, unsafe_allow_html=True)
 
+
+Titulo_principal = '<p style="font-family:TimesNewRoman; color:DarkBlue; font-size: 42px;">Óptica Cuántica</p>'
+st.markdown(Titulo_principal, unsafe_allow_html=True)
 
 st.caption('Esta aplicacón fue hecha con el propósito de introducir hacia los fenómenos cuánticos, especificamente dentro de la rama de la óptica cuántica, que ha sido de dificil comprensión para la cual nuestro objetivo es demostrar tales fenómenos con ejemplos visuales y prácticos de entender')
 
@@ -18,7 +23,7 @@ if Temas == 'Introducción':
     
     st.header('Introducción')
     st.subheader('**¿Qué es la luz?**')
-    st.markdown('Una pregunta que los seres humanos han tratado de responder durante siglos.')
+    st.markdown("Una pregunta que los seres humanos han tratado de responder durante siglos.")
     
     col1, col2, col3 = st.columns([1,6,1])
 
