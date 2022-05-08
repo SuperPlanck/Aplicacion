7d80caf01ac676de0ce6403d3d989148640b824c
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-07 18:12:18 -0600

    m

--- Aplicacion.py
+++ Aplicacion.py
@@ -5,8 +5,8 @@ import streamlit as st
 import numpy as np
 st.set_page_config(page_title="Óptica Cuántica",page_icon="🎇")
 
-Titulo_concurso = '<p style="text-align:center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
-st.markdown(Titulo_concurso, unsafe_allow_html=True)
+Titulo_c = '<p style="text-align:center";"font-family:Courier; color:Blue; font-size: 20px;">Hackaton-Quantum Apps</p>'
+st.markdown(Titulo_c, unsafe_allow_html=True)
 
 
 Titulo_principal = '<p style="font-family:TimesNewRoman; color:DarkBlue; font-size: 42px;">Óptica Cuántica</p>'
